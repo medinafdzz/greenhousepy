7825c611fc5cc40d3b932cacc5ae7b426717afb7
Medinafdzz <suso121163@gmail.com>
2024-11-20 16:28:44 +0100

    US3 - Red phase

--- test/test_greenhouse.py
+++ test/test_greenhouse.py
@@ -44,12 +44,23 @@ class TestGreenhouse(TestCase):
 
     @patch.object(Seesaw, attribute="moisture_read")
     @patch.object(GPIO, attribute="output")
-    def test_manage_sprinkler_should_be_turned_on(self,mock_sprinkler: Mock, mock_moisture_sensor: Mock):
+    def test_manage_sprinkler_should_be_turned_on_when_below_375(self,mock_sprinkler: Mock, mock_moisture_sensor: Mock):
         mock_moisture_sensor.return_value = 374
         system = Greenhouse()
         system.manage_sprinkler()
         mock_sprinkler.assert_called_with(system.SPRINKLER_PIN, True)
         self.assertTrue(system.is_sprinkler_on())
+
+    @patch.object(Seesaw, attribute="moisture_read")
+    @patch.object(GPIO, attribute="output")
+    def test_manage_sprinkler_should_be_turned_on_when_above_425(self, mock_sprinkler: Mock, mock_moisture_sensor: Mock):
+        mock_moisture_sensor.return_value = 426
+        system = Greenhouse()
+        system.manage_sprinkler()
+        mock_sprinkler.assert_called_with(system.SPRINKLER_PIN, True)
+        self.assertTrue(system.is_sprinkler_on())
+
+
 
 
 
