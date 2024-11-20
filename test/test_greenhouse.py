462f32430d0d4e02aae5b24ee49a0c59e1b8bdb2
Medinafdzz <suso121163@gmail.com>
2024-11-20 15:58:12 +0100

    US1 - Red phase

--- test/test_greenhouse.py
+++ test/test_greenhouse.py
@@ -16,3 +16,10 @@ class TestGreenhouse(TestCase):
         moisture = system.measure_soil_moisture()
         self.assertEqual(300, moisture)
 
+    @patch.object(Seesaw, attribute='moisture_read')
+    def test_measure_soil_moisture_below_range(self, mock_moisture_sensor: Mock):
+        mock_moisture_sensor.return_value = 299
+        system = Greenhouse()
+        moisture = system.measure_soil_moisture()
+        self.assertRaises(299, system.measure_soil_moisture())
+
